a69e8cfd80ccc6af95a9637845e6a397ed02b261
Katerina3337 <katerina_samkova@mail.ru>
2023-03-13 20:02:06 +0300

    список карточек вынесен в отдельный компонент

---
+++ src/components/CardList.vue
@@ -0,0 +1,157 @@
+<template>
+  <ul v-if="cardList && cardList.length" class="cards">
+    <li
+      v-for="card in cardList"
+      :key="card.id"
+      @click="editCard(card, col.name)"
+      class="card"
+    >
+      <div class="card_name">{{ card.name }}</div>
+      <div class="card_summary">{{ card.summery }}</div>
+      <font-awesome-icon
+        @click.stop="openCardModal(card.id)"
+        icon="fa-solid fa-trash"
+        title="Удалить задачу"
+        class="card-icon"
+      />
+    </li>
+    <tr-modal
+      btnText="Удалить задачу"
+      :isOpened="createCardModal"
+      :cancel-action="true"
+      @mSubmit="removeCard"
+      @mClose="closeCardModal"
+    />
+  </ul>
+  <tr-modal
+    :hide-submit="true"
+    btnText="Добавить задачу"
+    :isOpened="createModal"
+    :modalWidth="1000"
+    @mClose="closeModal"
+    class="modal-edit"
+  >
+    <template v-slot:modal-body>
+      <card-edit
+        :card="cardEdited"
+        :col-name="cardEditedColName"
+        @update-card="updateCard"
+      />
+    </template>
+  </tr-modal>
+  <create-card :column-id="colId" @card-created="addCard" />
+</template>
+
+<script setup>
+import CreateCard from "@/components/CreateCard.vue";
+import CardEdit from "@/components/CardEdit.vue";
+import TrModal from "@/components/kit/TrModal.vue";
+import useAuthStore from "@/stores/auth";
+import { deleteCard, getCardList } from "@/api/api";
+import { ref, defineProps, defineEmits, onMounted } from "vue";
+
+const auth = useAuthStore();
+
+const props = defineProps(["colId", "col"]);
+const emit = defineEmits([]);
+
+const cardEdited = ref(null);
+const cardEditedColName = ref("update-columns");
+
+const createModal = ref(false);
+const createCardModal = ref(false);
+
+const deleteCardId = ref(null);
+
+const cardList = ref(null);
+onMounted(async () => {
+  if (!props.colId) return;
+
+  const resp = await getCardList(props.colId, auth.token);
+
+  cardList.value = resp.data;
+  console.log(cardList);
+});
+
+const openModal = () => {
+  createModal.value = true;
+};
+const closeModal = () => {
+  createModal.value = false;
+};
+
+const openCardModal = (cardId) => {
+  createCardModal.value = true;
+  deleteCardId.value = cardId;
+};
+
+const closeCardModal = () => {
+  createCardModal.value = false;
+};
+
+const editCard = (card, columnName) => {
+  openModal();
+  cardEdited.value = card;
+  cardEditedColName.value = columnName;
+};
+
+const updateCard = (upCard) => {
+  const updatedCard = cardList.value.find((item) => {
+    return item.id === upCard.id;
+  });
+  updatedCard.summery = upCard.summery;
+  updatedCard.description = upCard.description;
+  closeModal();
+
+  emit("update-columns");
+};
+
+const addCard = (card) => {
+  cardList.value.push(card);
+};
+
+const removeCard = async () => {
+  const resp = await deleteCard(deleteCardId.value, auth.token);
+
+  if (resp.status === 200) {
+    const cardIdx = cardList.value.findIndex((item) => {
+      return item.id === deleteCardId.value;
+    });
+
+    cardList.value.splice(cardIdx, 1);
+  }
+  closeCardModal();
+};
+</script>
+
+<style scoped>
+.cards {
+  padding: 0;
+  list-style: none;
+}
+.card {
+  margin-bottom: 15px;
+  padding: 3px 7px;
+  position: relative;
+  color: #5d5a88;
+  background-color: #d4d2e3;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.card_name {
+  font-weight: bold;
+}
+
+.card_summary {
+  margin-right: 3px;
+}
+
+.card-icon {
+  display: inline-block;
+  position: absolute;
+  right: 5px;
+  bottom: 5px;
+  margin-left: 5px;
+}
+</style>
--- src/components/ColumnItem.vue
+++ src/components/ColumnItem.vue
@@ -14,56 +14,15 @@
       @mSubmit="removeColumn(column.id)"
       @mClose="closeColModal"
     />
+    <card-list :colId="column.id" :col="column"></card-list>
   </div>
-  <ul v-if="cardList && cardList.length" class="cards">
-    <li
-      v-for="card in cardList"
-      :key="card.id"
-      @click="editCard(card, column.name)"
-      class="card"
-    >
-      <div class="card_name">{{ card.name }}</div>
-      <div class="card_summary">{{ card.summery }}</div>
-      <font-awesome-icon
-        @click.stop="openCardModal"
-        icon="fa-solid fa-trash"
-        title="Удалить задачу"
-        class="card-icon"
-      />
-    </li>
-    <tr-modal
-      btnText="Удалить задачу"
-      :isOpened="createCardModal"
-      :cancel-action="true"
-      @mSubmit="removeCard(card.id)"
-      @mClose="closeCardModal"
-    />
-  </ul>
-  <tr-modal
-    :hide-submit="true"
-    btnText="Добавить задачу"
-    :isOpened="createModal"
-    :modalWidth="1000"
-    @mClose="closeModal"
-    class="modal-edit"
-  >
-    <template v-slot:modal-body>
-      <card-edit
-        :card="cardEdited"
-        :col-name="cardEditedColName"
-        @update-card="updateCard"
-      />
-    </template>
-  </tr-modal>
-  <create-card :column-id="column.id" @card-created="addCard" />
 </template>
 
 <script setup>
-import { defineProps, defineEmits, onMounted, ref } from "vue";
-import { getCardList, deleteColumn, deleteCard } from "@/api/api";
-import CreateCard from "@/components/CreateCard.vue";
+import { defineProps, ref } from "vue";
+import { deleteColumn } from "@/api/api";
+import CardList from "@/components/CardList.vue";
 import TrModal from "@/components/kit/TrModal.vue";
-import CardEdit from "@/components/CardEdit.vue";
 
 import useAppStore from "@/stores/app";
 const app = useAppStore();
@@ -71,29 +30,9 @@ const app = useAppStore();
 import useAuthStore from "@/stores/auth";
 const auth = useAuthStore();
 
-const props = defineProps(["column"]);
-const emit = defineEmits(["update-columns"]);
+defineProps(["column"]);
 
-const cardList = ref(null);
-const createModal = ref(false);
 const useModal = ref(false);
-const createCardModal = ref(false);
-const cardEdited = ref(null);
-const cardEditedColName = ref("");
-
-onMounted(async () => {
-  if (!props.column.id) return;
-
-  const resp = await getCardList(props.column.id, auth.token);
-
-  cardList.value = resp.data;
-});
-const openModal = () => {
-  createModal.value = true;
-};
-const closeModal = () => {
-  createModal.value = false;
-};
 
 const openColModal = () => {
   useModal.value = true;
@@ -101,48 +40,6 @@ const openColModal = () => {
 
 const closeColModal = () => {
   useModal.value = false;
-};
-
-const openCardModal = () => {
-  createCardModal.value = true;
-};
-
-const closeCardModal = () => {
-  createCardModal.value = false;
-};
-
-const editCard = (card, columnName) => {
-  openModal();
-  cardEdited.value = card;
-  cardEditedColName.value = columnName;
-};
-
-const updateCard = (upCard) => {
-  const updatedCard = cardList.value.find((item) => {
-    return item.id === upCard.id;
-  });
-  updatedCard.summery = upCard.summery;
-  updatedCard.description = upCard.description;
-  closeModal();
-
-  emit("update-columns");
-};
-
-const addCard = (card) => {
-  cardList.value.push(card);
-};
-
-const removeCard = async (id) => {
-  const resp = await deleteCard(id, auth.token);
-
-  if (resp.status === 200) {
-    const cardIdx = cardList.value.findIndex((item) => {
-      return item.id === id;
-    });
-
-    cardList.value.splice(cardIdx, 1);
-  }
-  closeColModal();
 };
 
 const removeColumn = async (id) => {
@@ -155,7 +52,7 @@ const removeColumn = async (id) => {
 
     app.columnList.splice(columnIdx, 1);
   }
-  closeCardModal();
+  closeColModal();
 };
 </script>
 
@@ -174,35 +71,5 @@ const removeColumn = async (id) => {
 
 .column-title {
   margin-bottom: 15px;
-}
-
-.cards {
-  padding: 0;
-  list-style: none;
-}
-.card {
-  margin-bottom: 15px;
-  padding: 3px 7px;
-  position: relative;
-  color: #5d5a88;
-  background-color: #d4d2e3;
-  border-radius: 5px;
-  cursor: pointer;
-}
-
-.card_name {
-  font-weight: bold;
-}
-
-.card_summary {
-  margin-right: 3px;
-}
-
-.card-icon {
-  display: inline-block;
-  position: absolute;
-  right: 5px;
-  bottom: 5px;
-  margin-left: 5px;
 }
 </style>
